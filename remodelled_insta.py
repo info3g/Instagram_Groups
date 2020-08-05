6de3599c08f1f524367bae311ad9a8fbde8fd557
info3g <infothreeg@gmail.com>
2020-08-04 23:58:14 -0700

    new commit

--- remodelled_insta.py
+++ remodelled_insta.py
@@ -4,6 +4,7 @@ import os,time,pandas,xlrd
 from bs4 import BeautifulSoup as bs
 from selenium.webdriver.common.keys import Keys
 import pymysql.cursors
+import random
 # chrome_options = Options()
 # chrome_options.add_argument("--headless")
 # chrome_options.add_argument("--window-size=1920x1080")
@@ -19,9 +20,8 @@ import pymysql.cursors
 # user = driver.find_element_by_name("username")
 # user.send_keys('football___stars___')
 
-def instagram(user_name,passwd,excel_data_df,private_acc_list):
+def instagram(user_name,passwd,excel_data_df):
     ############################## LOGIN ########################################
-    print(private_acc_list)
     driver = webdriver.Chrome('chromedriver.exe')
 
     driver.get('https://www.instagram.com/accounts/login/')
@@ -40,123 +40,116 @@ def instagram(user_name,passwd,excel_data_df,private_acc_list):
     excel_data_df['Group A (player)'].dropna(inplace=True)
     excel_data_df['Group B (agent)'].dropna(inplace=True)
     list_user_a = excel_data_df['Group A (player)'].tolist()
-
     for user in list_user_a:
-        if user in private_acc_list:    
-            print(user)
-            all_following_a = []
-            driver.get('https://www.instagram.com/'+str(user)+'/')
+        print(user)
+        all_following_a = []
+        driver.get('https://www.instagram.com/'+str(user)+'/')
+        time.sleep(2)
+        try:
+            driver.find_element_by_xpath("/html[1]/body[1]/div[1]/section[1]/main[1]/div[1]/header[1]/section[1]/ul[1]/li[3]/a[1]").click()
+            following_num = driver.find_element_by_xpath("//li[3]//a[1]//span[1]").text
+            following_num = following_num.replace(',','')
+            following_num = int(following_num)
             time.sleep(2)
-            try:
-                driver.find_element_by_xpath("/html[1]/body[1]/div[1]/section[1]/main[1]/div[1]/header[1]/section[1]/ul[1]/li[3]/a[1]").click()
-                following_num = driver.find_element_by_xpath("//li[3]//a[1]//span[1]").text
-                following_num = following_num.replace(',','')
-                following_num = int(following_num)
-                time.sleep(2)
-                followings_panel = driver.find_element_by_xpath('//body/div/div/div/div[2]')
-                z=0
-                while True:
-                    driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight",followings_panel)
-                    z = z + 1
-                    time.sleep(0.5)
-                    if z > following_num / 10:
-                        break
-                soup = bs(driver.page_source)
-                all_divs = soup.find('div', {'class':'_1XyCr'})
-                usernames = all_divs.find_all('li')
-                for each_username in usernames:
-                    a_tag = each_username.find_all('a')
-                    for all_user in a_tag:
-                        all_following_a.append(all_user.text)
-                all_following_a = list(set(all_following_a))
+            followings_panel = driver.find_element_by_xpath('//body/div/div/div/div[2]')
+            z=0
+            while True:
+                driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight",followings_panel)
+                z = z + 1
+                time.sleep(0.5)
+                if z > following_num / 10:
+                    break
+            soup = bs(driver.page_source)
+            all_divs = soup.find('div', {'class':'_1XyCr'})
+            usernames = all_divs.find_all('li')
+            for each_username in usernames:
+                a_tag = each_username.find_all('a')
+                for all_user in a_tag:
+                    all_following_a.append(all_user.text)
+            all_following_a = list(set(all_following_a))
+            
+            All_user_B = list_user = excel_data_df['Group B (agent)'].tolist()
+            user_list = []
+            for each_user in all_following_a:
+                if each_user in All_user_B:
                 
-                All_user_B = list_user = excel_data_df['Group B (agent)'].tolist()
-                user_list = []
-                for each_user in all_following_a:
-                    if each_user in All_user_B:
-                    
-                        print("*****************************************************",each_user)
+                    print("*****************************************************",each_user)
 
-                        user_list.append(each_user)
-                        # user_dict.update({user : user_list})
-                    else:
-                        pass
-                if len(user_list) == 0:
-                    user_dict.update({user : ["None"]})
-                else:           
-                    user_dict.update({user : user_list})        
-                private_acc_list.remove(user)
-            except:
-                user_dict.update({user : ["None"]})#if the user is private
-                user_status =driver.find_element_by_xpath('//section//section//div//div[1]//button[1]').text
-                if user_status == 'Follow':#Follow the user
-                    followButton =driver.find_element_by_xpath('//section//section//div//div[1]//button[1]')
-                    followButton.click()
-                elif user_status == 'Requested':#pass if we have already sent the follow request
+                    user_list.append(each_user)
+                    # user_dict.update({user : user_list})
+                else:
                     pass
+            if len(user_list) == 0:
+                user_dict.update({user : ["None"]})
+            else:           
+                user_dict.update({user : user_list})        
+            
+        except:
+            user_dict.update({user : ["None"]})#if the user is private
+            user_status =driver.find_element_by_xpath('//section//section//div//div[1]//button[1]').text
+            if user_status == 'Follow':#Follow the user
+                followButton =driver.find_element_by_xpath('//section//section//div//div[1]//button[1]')
+                followButton.click()
+            elif user_status == 'Requested':#pass if we have already sent the follow request
+                pass
         #################################################################       
     # list_user = excel_data_df['Group A (player)'].tolist()
     # if list_user[-1] == user:
     All_user_B = excel_data_df['Group B (agent)'].tolist()
-
     for user in All_user_B:
-        if user in private_acc_list:
-            all_following_b = []
-            driver.get('https://www.instagram.com/'+str(user)+'/')
-            time.sleep(5)
-            try:
-                driver.find_element_by_xpath("/html[1]/body[1]/div[1]/section[1]/main[1]/div[1]/header[1]/section[1]/ul[1]/li[3]/a[1]").click()
-                follower_num = driver.find_element_by_xpath("//li[3]//a[1]//span[1]").text
-                follower_num = follower_num.replace(',','')
-                follower_num = int(follower_num)
-                time.sleep(2)
-                following_panel = driver.find_element_by_xpath('//body/div/div/div/div[2]')
-                z=0
-                while True:
-                    driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight",following_panel)
-                    z = z + 1
-                    time.sleep(0.5)
-                    if z > follower_num / 10:
-                        break
-                soup = bs(driver.page_source)
-                all_divs = soup.find('div', {'class':'_1XyCr'})
-                usernames = all_divs.find_all('li')
-                for each_username in usernames:
-                    a_tag = each_username.find_all('a')
-                    for all_user in a_tag:
-                        all_following_b.append(all_user.text)
-                all_following_b = list(set(all_following_b))
-                All_user_A = list_user = excel_data_df['Group A (player)'].tolist()
-                user_list = []
-                for each_user in all_following_b:
-                    
-                    if each_user in All_user_A:
-                        print("*****************************************************",each_user)
-                        user_list.append(each_user)
-                        # print("Match User BBBbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",All_user_A)
-                        # user_dict.update({user : user_list})
-                    else:
-                        pass
+        all_following_b = []
+        driver.get('https://www.instagram.com/'+str(user)+'/')
+        time.sleep(5)
+        try:
+            driver.find_element_by_xpath("/html[1]/body[1]/div[1]/section[1]/main[1]/div[1]/header[1]/section[1]/ul[1]/li[3]/a[1]").click()
+            follower_num = driver.find_element_by_xpath("//li[3]//a[1]//span[1]").text
+            follower_num = follower_num.replace(',','')
+            follower_num = int(follower_num)
+            time.sleep(2)
+            following_panel = driver.find_element_by_xpath('//body/div/div/div/div[2]')
+            z=0
+            while True:
+                driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight",following_panel)
+                z = z + 1
+                time.sleep(0.5)
+                if z > follower_num / 10:
+                    break
+            soup = bs(driver.page_source)
+            all_divs = soup.find('div', {'class':'_1XyCr'})
+            usernames = all_divs.find_all('li')
+            for each_username in usernames:
+                a_tag = each_username.find_all('a')
+                for all_user in a_tag:
+                    all_following_b.append(all_user.text)
+            all_following_b = list(set(all_following_b))
+            All_user_A = list_user = excel_data_df['Group A (player)'].tolist()
+            user_list = []
+            for each_user in all_following_b:
                 
-                if len(user_list) == 0:
-                    user_dict.update({user : ["None"]})
-                else:   
-                    user_dict.update({user : user_list})    
-                private_acc_list.remove(user)    
-            except:
-                user_dict.update({user : ["None"]})
-                private_acc_list.append(user)
-                user_status =driver.find_element_by_xpath('//section//section//div//div[1]//button[1]').text
-
-                if user_status == 'Follow':
-                    followButton =driver.find_element_by_xpath('//section//section//div//div[1]//button[1]')
-                    followButton.click()
-                elif user_status == 'Requested':
+                if each_user in All_user_A:
+                    print("*****************************************************",each_user)
+                    user_list.append(each_user)
+                    # print("Match User BBBbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",All_user_A)
+                    # user_dict.update({user : user_list})
+                else:
                     pass
+            
+            if len(user_list) == 0:
+                user_dict.update({user : ["None"]})
+            else:   
+                user_dict.update({user : user_list})    
+        except:
+            user_dict.update({user : ["None"]})
+            user_status =driver.find_element_by_xpath('//section//section//div//div[1]//button[1]').text
+            if user_status == 'Follow':
+                followButton =driver.find_element_by_xpath('//section//section//div//div[1]//button[1]')
+                followButton.click()
+            elif user_status == 'Requested':
+                pass
     driver.close()
     print(user_dict)
     print(excel_data_df)
-    return user_dict,excel_data_df,private_acc_list
+    return user_dict,excel_data_df
     
 def save_to_db(user_dict,excel_data_df):
 #     print(user_dict)
@@ -232,11 +225,31 @@ def save_to_db(user_dict,excel_data_df):
 #     res = c.fetchall()
 #     print(res)    
 
-def check_user_dict(first_user_dict,second_user_dict):
-    correct_dict={}
-    for (k1,v1),(k2,v2) in zip(first_user_dict.items(),second_user_dict.items()):
-        if 'None' in v1 and 'None' not in v2:
-            v1 = v2
+
+def follow_on_submit(user_list):
+    for user in user_list:
+        driver = webdriver.Chrome('chromedriver.exe')
+
+        driver.get('https://www.instagram.com/accounts/login/')
+        time.sleep(2)
+        driver.find_element_by_name('username').send_keys(user_name)
+        time.sleep(2)
+        driver.find_element_by_name('password').send_keys(passwd)
+        driver.find_element_by_xpath("//button[contains(.,'Log In')]").click()
+        print("//////////////////////////////////")
+        time.sleep(4)
+        driver.find_element_by_xpath("//button[contains(.,'Not Now')]").click()
+        time.sleep(4)
+        driver.find_element_by_xpath("//button[contains(.,'Not Now')]").click()
+
+        driver.get('https://www.instagram.com/'+str(user)+'/')
+
+        user_status = driver.find_element_by_xpath('//section//section//div//div[1]//button[1]').text
+        if user_status == 'Follow':
+            followButton =driver.find_element_by_xpath('//section//section//div//div[1]//button[1]')
+            followButton.click()
+        elif user_status == 'Requested':
+            pass
 
 
 
@@ -244,40 +257,43 @@ def check_user_dict(first_user_dict,second_user_dict):
 if __name__ == '__main__':
     constants_df = pandas.read_excel('constants.xlsx')
     excel_data_df = pandas.read_excel('Agent_Player_List.xlsx')
-    users_a = excel_data_df['Group A (player)'].tolist()
-    users_b = excel_data_df['Group B (agent)'].tolist()
-    private_acc_list=users_a + users_b
-    # print(private_acc_list)
+    credentials_list = [[constants_df['Credentials_un'][0],constants_df['passwd'][0]],
+                        [constants_df['Credentials_un'][1],constants_df['passwd'][1]],
+                        [constants_df['Credentials_un'][2],constants_df['passwd'][2]]]
 
-    #1st ID
-    user_name = constants_df['Credentials_un'][0]
-    passwd = constants_df['passwd'][0]
+    index = random.randint(0,2)
+    user_name = credentials_list[index][0]
+    passwd = credentials_list[index][1]
+    # print(f'{user_name}    {passwd}')                    
+    # #1st ID
+    # user_name = constants_df['Credentials_un'][0]
+    # passwd = constants_df['passwd'][0]
     s = time.time()
-    user_dict,excel_data_df,private_acc_list = instagram(user_name,passwd,excel_data_df,private_acc_list)
+    user_dict,excel_data_df = instagram(user_name,passwd,excel_data_df)
     f = time.time()
-    print(f-s)
+    print(f'time taken: {f-s} secs')
     save_to_db(user_dict,excel_data_df)
-    print('First ID executed')
+    # print('First ID executed')
 
-    #2nd ID
-    user_name_two = constants_df['Credentials_un'][1]
-    passwd_two = constants_df['passwd'][1]
-    s = time.time()
-    user_dict,excel_data_df,private_acc_list = instagram(user_name,passwd,excel_data_df,private_acc_list)
-    f = time.time()
-    print(f-s)
-    save_to_db(user_dict,excel_data_df)
-    print('Second ID executed')
+    # #2nd ID
+    # user_name_two = constants_df['Credentials_un'][1]
+    # passwd_two = constants_df['passwd'][1]
+    # s = time.time()
+    # user_dict,excel_data_df = instagram(user_name,passwd,excel_data_df)
+    # f = time.time()
+    # print(f-s)
+    # save_to_db(user_dict,excel_data_df)
+    # print('Second ID executed')
 
-    #3rd ID
-    user_name_three = constants_df['Credentials_un'][2]
-    passwd_three = constants_df['passwd'][2]
-    s = time.time()
-    user_dict,excel_data_df,private_acc_list = instagram(user_name,passwd,excel_data_df,private_acc_list)
-    f = time.time()
-    print(f-s)
-    save_to_db(user_dict,excel_data_df)
-    print('Third ID executed')
+    # #3rd ID
+    # user_name_three = constants_df['Credentials_un'][2]
+    # passwd_three = constants_df['passwd'][2]
+    # s = time.time()
+    # user_dict,excel_data_df = instagram(user_name,passwd,excel_data_df)
+    # f = time.time()
+    # print(f-s)
+    # save_to_db(user_dict,excel_data_df)
+    # print('Third ID executed')
       
     # user_dict = {'moussa.cisse3': ['mt93290', 'mhn.93t'], 'wilson_smk': ['None'], 'momoo.el': ['None'], 'Matthis.abline': ['None'], 'philnabe': ['None'], 'availlant24': ['None'], 'guerra.frederic': ['None'], 'robindes_s': ['None'], 'mhn.93t': ['None'], 'mt93290': ['None']}
     # save_to_db(user_dict,excel_data_df)
